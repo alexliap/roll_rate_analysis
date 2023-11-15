19dcf11fb793e23f7742286ca741730a5b9e932a
alexliap <alexandrosliapates@gmail.com>
2023-11-15 09:46:16 +0200

    Added basic snapshot roll rate table class & methods, modified MOMRollRateTable class and .gitignore

--- src/roll_rate_analysis/mom_roll_rate_table.py
+++ src/roll_rate_analysis/mom_roll_rate_table.py
@@ -5,19 +5,13 @@ import numpy as np
 class MOMRollRateTable:
     def __init__(self, unique_key_col: str, delinquency_col: str, df1: pl.DataFrame, df2: pl.DataFrame, max_delq: int = 6):
         
-        # self.data_primary = df1
-        # self.data_secondary = df2
         self.unique_key_col = unique_key_col
         self.delinquency_col = delinquency_col
         self.data = df1.join(df2, how = 'left', on = self.unique_key_col, suffix = '_secondary')
         self.max_delq = max_delq
-        # self.schema_cols = ['up_to_date', '1_cycle_deliquent', '2_cycle_deliquent', '3_cycle_deliquent', 
-        #                     '4_cycle_deliquent', '5_cycle_deliquent', '6+_cycle_deliquent']
         self.roll_rate_matrix = np.zeros([self.max_delq + 1, self.max_delq + 1], dtype = np.int32)
 
     def build(self):
-        # data = self.data_primary.join(self.data_secondary, how = 'left', on = self.unique_key_col, suffix = '_secondary')
-
         for cycle in range(self.data[self.delinquency_col].min(), self.data[self.delinquency_col].max() + 1):
             self.roll_rate_matrix = self._n_cycle_performance(self.data, cycle = cycle)
         
@@ -31,14 +25,14 @@ class MOMRollRateTable:
 
         idxs = tmp.filter((pl.col(self.delinquency_col + '_secondary') <= (self.max_delq - 1)))[self.delinquency_col + '_secondary'].to_numpy()
         values = tmp.filter((pl.col(self.delinquency_col + '_secondary') <= (self.max_delq - 1)))['count'].to_numpy()
-        values_6_plus = tmp.filter((pl.col(self.delinquency_col + '_secondary') > (self.max_delq - 1)))['count'].sum()
+        values_plus = tmp.filter((pl.col(self.delinquency_col + '_secondary') > (self.max_delq - 1)))['count'].sum()
 
         if cycle >= self.max_delq:
             self.roll_rate_matrix[self.max_delq, idxs] += values
-            self.roll_rate_matrix[self.max_delq, self.max_delq] += values_6_plus
+            self.roll_rate_matrix[self.max_delq, self.max_delq] += values_plus
         else:
             self.roll_rate_matrix[cycle, idxs] += values
-            self.roll_rate_matrix[cycle, self.max_delq] += values_6_plus
+            self.roll_rate_matrix[cycle, self.max_delq] += values_plus
         
         return self.roll_rate_matrix
 
--- src/roll_rate_analysis/snapshot_roll_rate_table.py
+++ src/roll_rate_analysis/snapshot_roll_rate_table.py
@@ -3,33 +3,55 @@ import numpy as np
 import polars.selectors as cs
 
 class SnapshotRollRateTable:
-    def __init__(self, snapshot_file: str, unique_key_col: str, delinquency_col: str, obs_files: list[str], perf_files: list[str], keep_cols: list[str] = None):
+    def __init__(self, snapshot_file: str, unique_key_col: str, delinquency_col: str, obs_files: list[str], perf_files: list[str], keep_cols: list[str] = None, max_delq: int = 6):
         
         self.unique_key_col = unique_key_col
         self.delinquency_col = delinquency_col
+        self.max_delq = max_delq
         self.main_file = pl.scan_csv(source=snapshot_file).select([self.unique_key_col])
         self.obs_files = dict()
         self.perf_files = dict()
 
         for file in obs_files:
             self.obs_files[file] = pl.scan_csv(file)
-            if keep_cols is not None:
-                self.obs_files[file] = self.obs_files[file].select(keep_cols)
 
         for file in perf_files:
             self.perf_files[file] = pl.scan_csv(file)
-            if keep_cols is not None:
-                self.perf_files[file] = self.perf_files[file].select(keep_cols)
+
+        if keep_cols is not None:
+            self.keep(keep_cols)
 
         self.obs_data = None
         self.perf_data = None
 
-    def keep(self, cols_to_keep: list[str]):
-        for item in self.obs_files.keys():
-            self.obs_files[item] = self.obs_files[item].select(cols_to_keep)
+        self.roll_rate_matrix = np.zeros([self.max_delq + 1, self.max_delq + 1], dtype = np.int32)
 
-        for item in self.perf_files.keys():
-            self.perf_files[item] = self.perf_files[item].select(cols_to_keep)
+    def compute(self):
+        # most propably create new object RollRateTable with attributes which indicate the important columns and stuff
+        data = self.build().collect()
+        for cycle in range(data['obs_max_delq'].min(), data['obs_max_delq'].max() + 1):
+            self.roll_rate_matrix = self._n_cycle_performance(data, cycle = cycle)
+        
+        return pl.from_numpy(self.roll_rate_matrix, orient='row')
+
+    def _n_cycle_performance(self, data: pl.DataFrame, cycle: int):
+        tmp = (data.filter(pl.col('obs_max_delq') == cycle)
+               .groupby(['obs_max_delq', 'perf_max_delq'])
+               .count()
+               .sort('perf_max_delq'))
+
+        idxs = tmp.filter((pl.col('perf_max_delq') <= (self.max_delq - 1)))['perf_max_delq'].to_numpy()
+        values = tmp.filter((pl.col('perf_max_delq') <= (self.max_delq - 1)))['count'].to_numpy()
+        values_plus = tmp.filter((pl.col('perf_max_delq') > (self.max_delq - 1)))['count'].sum()
+
+        if cycle >= self.max_delq:
+            self.roll_rate_matrix[self.max_delq, idxs] += values
+            self.roll_rate_matrix[self.max_delq, self.max_delq] += values_plus
+        else:
+            self.roll_rate_matrix[cycle, idxs] += values
+            self.roll_rate_matrix[cycle, self.max_delq] += values_plus
+        
+        return self.roll_rate_matrix
 
     def build(self):
         self._build_obs_part()
@@ -39,7 +61,6 @@ class SnapshotRollRateTable:
         result = result.select([self.unique_key_col, 'obs_max_delq', 'perf_max_delq'])
 
         return result
-
 
     def _build_obs_part(self):
         self.obs_data = self.main_file
@@ -54,3 +75,10 @@ class SnapshotRollRateTable:
             self.perf_data = self.perf_data.join(self.perf_files[item], how = 'left', on = self.unique_key_col, suffix = '_' + item)
 
         self.perf_data = self.perf_data.with_columns(pl.max_horizontal(cs.starts_with(self.delinquency_col)).alias('perf_max_delq'))
+
+    def keep(self, cols_to_keep: list[str]):
+        for item in self.obs_files.keys():
+            self.obs_files[item] = self.obs_files[item].select(cols_to_keep)
+
+        for item in self.perf_files.keys():
+            self.perf_files[item] = self.perf_files[item].select(cols_to_keep)
