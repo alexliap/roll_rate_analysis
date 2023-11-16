6c65d3a2f7b5cdeb6eaf6523c63622c10938440f
alexliap <alex82009@windowslive.com>
2023-11-16 23:46:17 +0200

    Add info to setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,25 @@ from setuptools import find_packages, setup
 
 install_requires = ["numpy", "polars==0.19.11", "pandas"]
 
+extras_require = {"dev": ["pytest", "pre-commit", "jupyter"]}
+
 setup(
     name="roll_rate_analysis",
-    version="0.0.1",
+    version="0.1.0",
     package_dir={"": "src"},
     author="Alexandros Liapatis",
     author_email="alexanrosliapates@gmail.com",
     packages=find_packages(),
     install_requires=install_requires,
+    python_requires=">=3.8",
+    classifiers=[
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+        "Topic :: Credit Risk" "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    project_urls={"Source": "https://github.com/alexliap/roll_rate_analysis"},
 )
