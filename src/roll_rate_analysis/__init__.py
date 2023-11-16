7078990208f8315f56aea1591a49e6550504fc18
alexliap <alex82009@windowslive.com>
2023-11-16 23:32:38 +0200

    pre-commit __init__.py

--- src/roll_rate_analysis/__init__.py
+++ src/roll_rate_analysis/__init__.py
@@ -1,4 +1,4 @@
-from roll_rate_analysis.mom_roll_rate_table import MOMRollRateTable 
+from roll_rate_analysis.mom_roll_rate_table import MOMRollRateTable
 from roll_rate_analysis.snapshot_roll_rate_table import SnapshotRollRateTable
 
 __all__ = (MOMRollRateTable, SnapshotRollRateTable)
