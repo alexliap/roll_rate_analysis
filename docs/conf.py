0f28e6e7d2b4420373dcd0336ca67529d4675774
alexliap <alexandrosliapates@gmail.com>
2023-12-06 12:40:16 +0200

    test

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../src/roll_rate_analysis"))
+sys.path.insert(0, os.path.abspath("../src"))
 
 project = "Roll Rate Analysis"
 copyright = "2023, Alexandros Liapatis"
